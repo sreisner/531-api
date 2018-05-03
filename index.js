569bd49c1ce641ec06ccfd43c6d0f6b270b1867c
sreisner <shawn.reisner@gmail.com>
2018-05-03 16:37:11 -0500

    Remove test endpoints

--- index.js
+++ index.js
@@ -8,17 +8,4 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.get('/users/:userId', (req, res) => {
-    res.json({
-        userId: 123
-    });
-});
-
-app.get('/training-maxes', (req, res) => res.json({
-    press: 135,
-    bench: 225,
-    squat: 315,
-    deadlift: 405
-}));
-
 app.listen(3001, () => console.log('Running...'));
