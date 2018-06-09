77b5035298471286f1b58679745fcce40fd6d386
sreisner <shawn.reisner@gmail.com>
2018-06-09 13:30:13 -0500

    Validate /register post body

--- endpoints/register/register.js
+++ endpoints/register/register.js
@@ -2,9 +2,22 @@ const passport = require('passport');
 const bcrypt = require('bcrypt');
 const { User } = require('../../db/models');
 
+const emailContainsExactlyOneAtSign = email =>
+  email.split('').reduce((acc, curr) => (curr === '@' ? ++acc : acc), 0) === 1;
+
 const createEndpoints = router => {
   router.route('/register').post((req, res, next) => {
     const { firstName, lastName, email, password } = req.body;
+
+    if (password.length < 8) {
+      return res
+        .status(400)
+        .send('Password must be at least 8 characters long');
+    }
+
+    if (!emailContainsExactlyOneAtSign(email)) {
+      return res.status(400).send('The email you entered is invalid');
+    }
 
     User.findOne({ email }, (err, user) => {
       if (err) {
